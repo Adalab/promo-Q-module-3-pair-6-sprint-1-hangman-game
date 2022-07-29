953ef80bc59f274eed0ab6cc644837129956e1e6
Maria Luisa Lufrano <marialuisa.lufrano@icloud.com>
2022-07-29 11:15:54 +0200

    add array and objects exercise

--- src/components/App.js
+++ src/components/App.js
@@ -3,19 +3,41 @@ import { useState } from 'react';
 
 function App() {
   const [numberOfErrors, setNumberOfErrors] = useState(0);
-  const handleClick = (ev) => {
-    ev.preventDefault();
-    setNumberOfErrors(numberOfErrors + 1);
-    console.log('soy el boton');
-  };
+  // const handleClick = (ev) => {
+  //   ev.preventDefault();
+  //   setNumberOfErrors(numberOfErrors + 1);
+  //   console.log('soy el boton');
+  // };
 
   const [lastLetter, setLastLetter] = useState('');
+  const [word, setWord] = useState('katakroker');
+  const [userLetters, setUserLetters] = useState([]);
+
+  const renderSolutionLetters = () => {
+    const wordLetters = word.split('');
+    return wordLetters.map((letter, i) => {
+      if(!userLetters.includes(letter)) {
+        return <li key={i} className="letter"></li>
+      } else {
+        return <li key={i} className="letter">{letter}</li>
+      }
+       
+    })
+  }
+
+  const renderErrorLetters = () => {
+    console.log(userLetters);
+     const filteredLetters =  userLetters.filter((letter) => !word.includes(letter))
+     //setNumberOfErrors()
+    return filteredLetters.map((letter) => <li className="letter">{letter}</li>)
+  }
 
   const handleLastLetter = (ev) => {
     console.log(ev.currentTarget.value);
     if (ev.currentTarget.value.search(/[a-zA-ZñÑáÁéÉíÍóÓúÚ]/) === 0) {
       // Letra buena
       setLastLetter(ev.currentTarget.value);
+      setUserLetters([... userLetters,ev.currentTarget.value ])
     } else {
       // Letra mala
       console.log('Letra mala', ev.currentTarget.value);
@@ -33,37 +55,23 @@ function App() {
         <section>
           <div className="solution">
             <h2 className="title">Solución:</h2>
-            <ul className="letters">
-              <li className="letter">k</li>
-              <li className="letter">a</li>
-              <li className="letter"></li>
-              <li className="letter">a</li>
-              <li className="letter">k</li>
-              <li className="letter">r</li>
-              <li className="letter"></li>
-              <li className="letter">k</li>
-              <li className="letter">e</li>
-              <li className="letter">r</li>
+            <ul className="letters">{renderSolutionLetters()}
             </ul>
           </div>
           <div className="error">
             <h2 className="title">Letras falladas:</h2>
             <ul className="letters">
-              <li className="letter">f</li>
-              <li className="letter">q</li>
-              <li className="letter">h</li>
-              <li className="letter">p</li>
-              <li className="letter">x</li>
+            {renderErrorLetters()}
             </ul>
           </div>
           <form className="form">
-            <label className="title" for="last-letter">
+            <label className="title" htmlFor="last-letter">
               Escribe una letra:
             </label>
             <input
-              autocomplete="off"
+              autoComplete="off"
               className="form__input"
-              maxlength="1"
+              maxLength="1"
               type="text"
               name="last-letter"
               id="last-letter"
@@ -71,9 +79,9 @@ function App() {
               value={lastLetter}
             />
           </form>
-          <button className="button" onClick={handleClick}>
+          {/* <button className="button" onClick={handleClick}>
             Incrementar
-          </button>
+          </button> */}
         </section>
         <section className={`dummy error-${numberOfErrors}`}>
           <span className="error-13 eye"></span>
